35d74a44f6934e6d5040924cee38a217452c7dd7
Daniel Hauck <mail@dhauck.eu>
2020-08-09 01:32:56 +0200

    Add gaming layer.

--- keyboards/planck/keymaps/solidtux/keymap.c
+++ keyboards/planck/keymaps/solidtux/keymap.c
@@ -18,13 +18,14 @@
 #include "muse.h"
 #include <virtser.h>
 
-enum planck_layers { _QWERTY, _LOWER, _RAISE, _ADJUST, _NUMPAD, _MOUSE };
+enum planck_layers { _QWERTY, _GAME, _LOWER, _RAISE, _ADJUST, _NUMPAD, _MOUSE };
 
 enum planck_keycodes { QWERTY = SAFE_RANGE, COLOR, HEATMAP, IMAGE, RGBANIM, LED_LEV, SCREENS };
 
 #define LOWER MO(_LOWER)
 #define RAISE MO(_RAISE)
 #define NUMPAD MO(_NUMPAD)
+#define GAME TG(_GAME)
 #define MOUSE MO(_MOUSE)
 #define ESCAPE MT(MOD_RALT, KC_ESC)
 #define LSHIFT MT(MOD_LSFT, KC_BSLS)
@@ -57,8 +58,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
 ),
 [_NUMPAD] = LAYOUT_planck_grid(
-    _______, _______, _______, _______, _______, _______, _______, KC_KP_7, KC_KP_8, KC_KP_9, KC_PPLS, _______,
-    _______, _______, _______, _______, _______, _______, _______, KC_KP_4, KC_KP_5, KC_KP_6, KC_PMNS, KC_NUMLOCK,
+    DF(_GAME), _______, _______, _______, _______, _______, _______, KC_KP_7, KC_KP_8, KC_KP_9, KC_PPLS, _______,
+    DF(_QWERTY), _______, _______, _______, _______, _______, _______, KC_KP_4, KC_KP_5, KC_KP_6, KC_PMNS, KC_NUMLOCK,
     _______, _______, _______, _______, _______, _______, _______, KC_KP_1, KC_KP_2, KC_KP_3, KC_PAST, _______,
     _______, _______, _______, _______, MOUSE,   _______, _______, KC_KP_0, KC_PDOT, KC_PEQL, KC_PSLS, _______
 ),
@@ -67,6 +68,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, _______, _______, _______, _______, _______, _______, _______, KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R,
     _______, _______, _______, _______, _______, _______, _______, _______, KC_BTN1, KC_BTN2, KC_BTN3, KC_BTN4,
     _______, _______, _______, _______, _______, _______, _______, _______, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R
+),
+[_GAME] = LAYOUT_planck_grid(
+    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
+    ESCAPE,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+    LSHIFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, RSHIFT,
+    NUMPAD,  KC_LCTL, KC_LALT, KC_LGUI, RAISE,   KC_SPC,  KC_SPC,  LOWER,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
 )
 };
 // clang-format on
@@ -84,6 +91,19 @@ layer_state_t layer_state_set_user(layer_state_t state) {
         rgb_matrix_config.mode = last_mode;
     }
     last_layer = layer;
+    return state;
+}
+
+layer_state_t default_layer_state_set_user(layer_state_t state) {
+    uint8_t layer = biton32(state);
+    switch (layer) {
+        case _GAME:
+            last_mode = RGB_MATRIX_CUSTOM_game;
+            break;
+        case _QWERTY:
+            last_mode = RGB_MATRIX_STARTUP_MODE;
+            break;
+    }
     return state;
 }
 
